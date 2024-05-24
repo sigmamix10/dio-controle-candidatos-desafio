a31402e220271fe2c65aa7c8221e3bdd0c8a5511
Rogério Alves <sigmamix10@gmail.com>
2024-05-24 18:20:34 -0300

    Desafio 02

--- dio-controle-candidatos/src/ProcessoSeletivo.java
+++ dio-controle-candidatos/src/ProcessoSeletivo.java
@@ -4,19 +4,33 @@ public class ProcessoSeletivo {
 
     public static void main(String[] args){
         System.out.println("Processo Seletivo");
-        selecaoCandidato();    
+        imprmirSelecionados();
 
 
     }
 
     //Métodos
+    static void imprmirSelecionados(){
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo os candidatos selecionados informando o indice do elemento!");
+
+        for(int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " é: " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
     static void selecaoCandidato(){
         String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
         double salarioBase = 2000.00;
 
-        while (candidatosSelecionados < 5) {
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
             String candidato = candidatos[candidatosAtual];
             double salarioPretendido = valorPretendido();
 
