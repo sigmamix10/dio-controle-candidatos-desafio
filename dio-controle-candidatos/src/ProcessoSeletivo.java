86a9bcb1b63fab42b9fcef34744d81269fce22f8
Rogério Alves <sigmamix10@gmail.com>
2024-05-24 09:27:15 -0300

    Implementacao 03

---
+++ dio-controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+
+    public static void main(String[] args){
+        System.out.println("Processo Seletivo");
+        selecaoCandidato();    
+
+
+    }
+
+    //Métodos
+    static void selecaoCandidato(){
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.00;
+
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou esse valor de salario: " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vada.");
+                candidatosSelecionados++;
+                
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.00;
+        
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    }
+}
